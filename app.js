3803a44ff37f62f2533d48b7c2760a5d4b068603
Joel Tshimanga Mayer Mukanya <tmjoelm4@gmail.com>
2021-11-11 15:17:51 +0200

    Did upload both files

---
+++ app.js
@@ -0,0 +1,10 @@
+// const per = require("./index.js");
+// console.log(per.name); 
+// To be able to transfer data via HTTP using node
+// const http = require("http");
+
+//1. function(req, res) {}
+// or Create a server
+http.createServer(function(req, res) {
+    // Your code
+}).listen(8080); // Port number
---
+++ index.js
@@ -0,0 +1,12 @@
+
+file.read("your file name", function process(data){
+    // Code
+});
+// Example 1
+let person = {
+    name: "John",
+    surname: "Lucas",
+    age: 27
+};
+
+module.exports = person;
